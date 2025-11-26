a549e6a33725df5f311a80b272529b0b1521468b
triet <tr.l.m.triet@gmail.com>
2025-11-26 17:58:22 +0100

    Add job_id for multirun environment

--- scripts/train_from_config.py
+++ scripts/train_from_config.py
@@ -2,11 +2,15 @@ import os
 import hydra
 import pytorch_lightning as pl
 from omegaconf import DictConfig
+from pathlib import Path
+import time
+import torch
 
 
 os.environ['HYDRA_FULL_ERROR'] = "1"
 @hydra.main(config_path="../configs", config_name="config_fsav994", version_base=None)
 def main(cfg:DictConfig):
+    torch.cuda.empty_cache()
     pl.seed_everything(333) # seed for reproducibility
 
     ## DATA ##
@@ -24,7 +28,18 @@ def main(cfg:DictConfig):
     # print(sum(p.numel() for p in litmodel.parameters() if p.requires_grad))
 
     trainer = hydra.utils.call(cfg.trainer)
+    
+    start = time.time()
     trainer.fit(litmodel, dm)
+    end = time.time()
+
+    # write training time
+    output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    with open( Path("./training_times.txt" ) , "a") as f: 
+        f.write(f"{output_dir} :\n")
+        f.write(f"Training time: {(end-start)/3600:.3f} hour \n") 
+        f.write("______________________________________________________\n")
+    trainer.save_checkpoint( Path(output_dir, "last_epoch.ckpt") )
 
 if __name__ == "__main__": 
     main()
